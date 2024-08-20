00ec8a24edf60147b362944852e172a2bb2d5785
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-20 19:15:03 +0530

    Error resolve

--- db/index.ts
+++ db/index.ts
@@ -1,10 +1,8 @@
 import { drizzle } from 'drizzle-orm/postgres-js'
 import postgres from 'postgres'
 import * as schema from './schema';
-import { ENV_VARS } from '@/lib/envVars';
 
-
-const connectionString = ENV_VARS.DATABASE_URL;
+const connectionString = JSON.stringify(process.env.DATABASE_URL);
 
 const client = postgres(connectionString)
 export const db = drizzle(client, {schema});
