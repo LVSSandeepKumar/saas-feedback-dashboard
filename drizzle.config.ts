11aca074a8702c63d8b8c48dba1d1c5f88deac26
LVSSandeepKumar <lvssandeepkumar12@gmail.com>
2024-08-20 19:23:32 +0530

    Error resolve again

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -7,6 +7,6 @@ export default defineConfig({
     prefix: 'supabase'
   },
   dbCredentials: {
-    url: ENV_VARS.DATABASE_URL
+    url: process.env.DATABASE_URL || "postgresql://postgres.slnvwyslmekwowhznnkb:Fd0eQZo0q3Xsyq2M@aws-0-ap-south-1.pooler.supabase.com:5432/postgres"
   }
 })
